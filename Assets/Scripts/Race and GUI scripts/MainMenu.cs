2453e1d698cbb9c0ba87bb12860ce2ae5ef9d23d
rsr19 <animation_project2013@hotmail.com>
2013-12-17 23:15:25 -0700

    Almost complete menu system
    I put together a menu system that is almost complete. The problem is
    that it only works with mouse clicks and not touch input.

---
+++ Assets/Scripts/Race and GUI scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	//an array to hold the button graphics
+	public Texture[] menuGraphics;
+	
+	//get the menu move script
+	private MenuMove menu;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		menu = GetComponent<MenuMove>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Debug.Log(menu);
+		
+	}
+	
+	void OnGUI()
+	{
+		GUI.DrawTexture(menu.buttonPos[0], menuGraphics[0]);
+		
+		//display the buttons
+		if(GUI.Button(menu.buttonPos[1], menuGraphics[1]))
+		{
+			menu.MenuSwitch("test");
+		}
+		
+		if(GUI.Button(menu.buttonPos[2], menuGraphics[2]))
+		{
+			//menu.MoveOnLeft(1);
+		}
+		
+		if(GUI.Button(menu.buttonPos[3], menuGraphics[3]))
+		{
+			//menu.MoveOnLeft(1);
+		}
+	}
+}
